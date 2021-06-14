b22cd638c8eece948db025d6c08cbc6645298da9
rKremser03 <robin.kremser03@htl-salzburg.ac.at>
2021-06-14 13:57:53 +0200

    Everything

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    private InputField userName;
+    private TextMeshProUGUI loadedName;
+
+    public NameData nameData;
+    public LoadScene loadScene;
+
+
+    void Start()
+    {
+        userName = FindObjectOfType<InputField>();
+    }
+
+    
+
+    public void SaveName()
+    {
+        nameData.name = "NameData";
+        nameData.yourName = userName.text;
+        loadScene.LoadNextScene();
+
+    }
+
+    public void LoadName()
+    {
+        if(loadedName == null)
+        {
+            loadedName = GameObject.Find("LoadedName").GetComponent<TextMeshProUGUI>();
+        }
+        loadedName.text = nameData.yourName;
+    }
+}
